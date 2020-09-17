3f3527eac82ea33ba6ed75544ce450ab4e48e1bc
Pamela Ferreira <pam.elfen@gmail.com>
2020-09-17 18:29:05 -0300

    DTO, Service

---
+++ backend/src/main/java/com/devsuperior/dspesquisa/services/GameService.java
@@ -0,0 +1,25 @@
+package com.devsuperior.dspesquisa.services;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.devsuperior.dspesquisa.dto.GameDTO;
+import com.devsuperior.dspesquisa.entities.Game;
+import com.devsuperior.dspesquisa.repositories.GameRepository;
+
+@Service
+public class GameService {
+
+	@Autowired
+	private GameRepository repository;
+	
+	@Transactional(readOnly = true)
+	public List<GameDTO> findAll() {
+		List<Game> list = repository.findAll();
+		return list.stream().map(x -> new GameDTO(x)).collect(collectors.tolist());
+		
+	}
+}
